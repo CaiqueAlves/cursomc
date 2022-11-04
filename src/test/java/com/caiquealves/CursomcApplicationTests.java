0b7f7fe3342265224772033b48b1742704a0c625
CaiqueAlves <caiquealves858@gmail.com>
2022-11-04 16:31:36 -0300

    Projeto criado

---
+++ src/test/java/com/caiquealves/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiquealves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
